7fb772308ecd0c9dd1cc3959bb59da41aef0c619
RafaelaTS14 <126721199+RafaelaTS14@users.noreply.github.com>
2023-03-02 20:15:43 -0300

    tipos de variáveis

---
+++ 7_dados_3.js
@@ -0,0 +1,16 @@
+let idade = 31; // tipo -> number
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;  //não se tem diferenciação entre números inteiros e decimais
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo); // tipo -> boolean *verdadeiro ou falso*
+
+console.log(typeof "Teste"); //tipo -> string
+
+/*
+boolean tem o nome dado em homenagem ao seu criador.
+em suma, é um type true ou false.
+*/
