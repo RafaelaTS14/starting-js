d67024eb88a2ff98b96e24da7672583e609b9561
RafaelaTS14 <rafa456.rts@gmail.com>
2023-03-06 11:59:08 -0300

    aula de algoritmo - início

---
+++ 0_algoritmo.js
@@ -0,0 +1,10 @@
+/* 
+Algoritmo é uma sequência de passos.
+É comum que o algoritmo tenha repetição.
+Repetição: você pode repetir "x" vezes.
+ou até uma determinada condição:
+"repetir até..."
+Também é possível tomar uma decisão.
+Um passo dependendo de outro ou ignorando um passo.
+
+*/
