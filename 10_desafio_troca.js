6fa9f8c7f135739908e0e7f46a6a81f526231431
RafaelaTS14 <rafa456.rts@gmail.com>
2023-03-06 11:29:48 -0300

    Troca de valor de variável

---
+++ 10_desafio_troca.js
@@ -0,0 +1,15 @@
+let a = 7;
+let b = 94;
+
+// let temp = a;
+
+// trocar o valor das variáveis
+
+// a = b;
+// b = temp;
+
+// outra forma de trocar o valor das variáveis é usando colchetes
+[a , b] = [b , a]
+
+console.log(a);
+console.log(b);
