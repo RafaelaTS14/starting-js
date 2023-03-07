be02fb85bfa6ff5e29cf5d03171ec87e6c39725e
RafaelaTS14 <rafa456.rts@gmail.com>
2023-03-07 11:15:44 -0300

    operações com números que não funcionam corretamente ou são perigosas

---
+++ 3_numeros_cuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0); // gera um tipo "infinito"
+console.log("10" / 2); //ele converte pra number e faz a operação, mas não deixa de ser estranho
+console.log("Show!" * 2); // não funciona
+console.log(0.1 + 0.7); //impreciso
+// console.log(10.toString); não funciona
+console.log((10.345).toFixed(2));
