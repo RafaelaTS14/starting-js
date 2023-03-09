32b471eb1b7b906f43c680dcafb6ba3cfdaed41f
RafaelaTS14 <rafa456.rts@gmail.com>
2023-03-09 14:26:10 -0300

    objeto

--- 9_object.js
+++ 9_object.js
@@ -1,5 +1,5 @@
-const prod1 = {};
-prod1.nome = 'Celular Ultra Mega';
+const prod1 = {}; //objeto vazio dentro da constante
+prod1.nome = 'Celular Ultra Mega'; //.nome é o atributo da constante, enquanto 'Celular Ultra Mega' é o valor do atributo
 prod1.preco = 4998.90;
 
 console.log(prod1);
