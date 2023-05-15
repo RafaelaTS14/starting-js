d2b85d2591186030a8bf4ef2151f2d3aff7993f5
Rafaela Taschner Shcimaia <rafa456.rts@gmail.com>
2023-05-15 14:18:33 -0300

    Fundamentos: atribuição, par nome/valor e notação ponto

--- Java script - fundamentos/23_parNomeValor.js
+++ Java script - fundamentos/23_parNomeValor.js
@@ -1,1 +1,20 @@
 const saudacao = 'Opa'
+
+function exec() {
+    const saudacao = 'Falaaaa'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
